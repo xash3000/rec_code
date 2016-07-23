434c7a0004a7a1636abb2759b04f6d68d8d99e35
mohamed-aziz <medazizknani@gmail.com>
2016-07-23 11:00:55 +0100

    More general case?

--- rec_rev.py
+++ rec_rev.py
@@ -6,15 +6,12 @@ recursively reverse array
 def rec_rev(arr):
     """
     rec_rev([1]) => [1]
-    rec_rev([1, 2]) => [2, 1]
-    rec_rev([1, 2, 3, 4, 5]) = > [5, rec_rev([2, 3, 4]) ,1]
+    rec_rev([1, 2, 3, 4, 5]) = > [rec_rev([2, 3, 4, 5]), 1]
     """
     if len(arr) == 1:
         return arr
-    elif len(arr) == 2:
-        return [arr[-1], arr[0]]
     else:
-        return [arr[-1]] + rec_rev(arr[1:-1]) + [arr[0]]
+        return rec_rev(arr[1:]) + [arr[0]]
 
 
 # test
